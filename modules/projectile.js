331e6a6d430e27c6d761ad66579f02a537872e51
Goya <84646090+Scarlote@users.noreply.github.com>
2023-05-23 11:36:04 -0500

    Update projectile.js

--- modules/projectile.js
+++ modules/projectile.js
@@ -93,4 +93,4 @@ export function pushProjectile() {
   projectileStorage.push(PROJECTILE)
 }
 
-setInterval(() => { pushProjectile() }, 125)
+window.onload = setInterval(() => { pushProjectile() }, 125)
